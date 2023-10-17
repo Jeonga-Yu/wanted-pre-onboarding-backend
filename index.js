56a6083791015b2632d5b8e1b25fe4fbda9efb5c
Jeonga-Yu <dev.j3rry@gmail.com>
2023-10-17 17:45:22 +0900

    Update index.js
    요구사항 구현
    : 채용공고 등록, 수정, 삭제, 조회

--- index.js
+++ index.js
@@ -21,3 +21,53 @@ connection.connect( (err) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// 1. 채용공고 등록
+app.put('/', (req, res) => {
+  const body = req.body
+  const sql =
+  'INSERT INTO test_db.recruit (id,position,compensation,detail,tech) VALUES(?,?,?,?,?)'
+
+  connection.query(sql, [body.id, body.position, body.compensation, body.detail, body.tech], (err, rows, field) => {
+    if (err) throw err
+    res.send(rows)
+  })
+})
+
+
+// 2. 수정
+app.post('/', (req, res) => {
+  const body = req.body
+  const sql =
+  'UPDATE test_db.recruit SET position=?,compensation=?,detail=?,tech=? WHERE id = ?'
+
+  connection.query(sql, [body.position, body.compensation, body.detail, body.tech, body.id], (err, rows, field) => {
+    if (err) throw err
+    res.redirect('/')
+  })
+})
+
+
+// 3. 삭제
+app.delete('/', (req, res) => {
+  const body = req.body
+  const sql =
+  'DELETE FROM test_db.recruit WHERE id = ?'
+
+  connection.query(sql, [body.id], (err, rows, field) => {
+    if (err) throw err
+    res.redirect('/')
+  })
+})
+
+
+// 4. 조회
+app.get('/', (req, res) => {
+  const sql = 'SELECT id, position, compensation, detail, tech FROM test_db.recruit'
+  
+  connection.query(sql, (err, rows, fields) => {
+    if (err) throw err
+    res.send(rows)
+  })
+  
+})
